fb7884918a7ff447aa214bb8ac20ae9f02014cf5
Victoria Bezazieva <victoria.bezazi@ngft.com>
2024-11-16 21:14:55 +0300

    changed title

--- pages/intro.js
+++ pages/intro.js
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <Layout>
       <Head>
-        <title>Orido | Intro</title>
+        <title>Leisan</title>
       </Head>
       <div className="orido_tm_intro">
         <div className="orido_tm_intro_fixed_price">
